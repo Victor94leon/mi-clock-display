dfe19a4fc9053d4afddf59e8458c82625c482b22
Víctor <victorlopez94leon@gmail.com>
2015-11-20 13:17:39 +0100

    Modificado para introducir y mostrar fecha

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -7,6 +7,12 @@
  *Tener un método setTime que acepte dos parámetros de tipo int, que representan horas y minutos, y que fije dichos valores como el tiempo actual del reloj.
  *Tener un método getTime que devuelva la hora como String de 5 caracteres. 
  *Tener un método timeTick que haga avanzar un minuto la hora actual (sea esta la que sea originalmente). 
+ *
+ *Modifica a partir de la actividad 027 el código de la clase ClockDisplay para que sea un reloj además muestre el año, el mes y el día. Para simplificar 
+ *vamos a imaginar que todos los meses tienen 30 días.
+ *
+ *El reloj debe poder ser construido indicando un momento temporal como el expresado anteriormente y el método setTime también debe permitir fijar un 
+ *momento temporal completo (es decir, no solo las horas y minutos).
  */
 
 public class ClockDisplay {
@@ -14,6 +20,12 @@ public class ClockDisplay {
     private NumberDisplay horas;
     // Display para los minutos
     private NumberDisplay minutos;
+    // Display para los años
+    private NumberDisplay años;
+    // Display para los meses
+    private NumberDisplay meses;
+    // Display para los días
+    private NumberDisplay dias;
     // Datos guardados en un String para mostar la hora
     private String timeNow;
     // Dato para guardar la opción de ver lahora en formato 24 ó 12
@@ -26,8 +38,13 @@ public class ClockDisplay {
     public ClockDisplay (boolean formato12)
     {
         formato = formato12;
+        
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(60);
+        años = new NumberDisplay(100);
+        meses = new NumberDisplay(12);
+        dias = new NumberDisplay(30);
+        
         updateHoraActual();
     }
     
@@ -35,25 +52,42 @@ public class ClockDisplay {
      * Crea una objeto de la clase ClockDisplay con dos parametros para fijar 
      * la hora y la opción de ver la hora en formato 12(true) ó 24(false)
      */
-    public ClockDisplay (int parHora,int parMinuto,boolean formato12)
+    public ClockDisplay (int parHora,int parMinuto,int parDia,int parMes,int parAño,boolean formato12)
     {
         formato = formato12;
+        
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(60);
+        años = new NumberDisplay(100);
+        meses = new NumberDisplay(12);
+        dias = new NumberDisplay(30);
+        
         horas.setValue(parHora);
         minutos.setValue(parMinuto);
+        años.setValue(parAño);
+        meses.setValue(parMes);
+        dias.setValue(parDia);
+        
         updateHoraActual();
     }
     
     /**
      * Método setter que fija los parámetros introducidos como lahora nueva actual
      */
-    public void setTime (int parHora,int parMinuto)
+    public void setTime (int parHora,int parMinuto,int parDia,int parMes,int parAño,boolean formato12)
     {
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(60);
+        años = new NumberDisplay(100);
+        meses = new NumberDisplay(12);
+        dias = new NumberDisplay(30);
+
         horas.setValue(parHora);
         minutos.setValue(parMinuto);
+        años.setValue(parAño);
+        meses.setValue(parMes);
+        dias.setValue(parDia);
+        
         updateHoraActual();
     }
     
@@ -73,6 +107,15 @@ public class ClockDisplay {
         minutos.increment();
         if (minutos.getValue() == 0) {
             horas.increment();
+            if (horas.getValue() == 0) {
+                dias.increment();
+                if (dias.getValue() == 0) {
+                    meses.increment();
+                    if (meses.getValue() == 0) {
+                        años.increment();
+                    }
+                }
+            }
         }
         updateHoraActual();
     }
@@ -84,10 +127,10 @@ public class ClockDisplay {
     public void updateHoraActual()
     {
         if (formato) {
-            String formatoReloj = "a.m";
+            String formatoReloj = "a.m ";
             int horaAhora = horas.getValue(); 
             if (horaAhora >= 12){
-                formatoReloj = "p.m.";
+                formatoReloj = "p.m. ";
             }
             
             if (horaAhora > 12) {
@@ -96,10 +139,10 @@ public class ClockDisplay {
             else if (horaAhora == 0) {
                 horaAhora = 12;
             }
-            timeNow = horaAhora + ":" + minutos.getDisplayValue() + " " + formatoReloj;
+            timeNow = horaAhora + ":" + minutos.getDisplayValue() + " " + formatoReloj + dias.getDisplayValue() + "/" + meses.getDisplayValue() + "/" + años.getDisplayValue();
         }
         else {
-            timeNow = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+            timeNow = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + dias.getDisplayValue() + "/" + meses.getDisplayValue() + "/" + años.getDisplayValue();;
         }
     }
     
